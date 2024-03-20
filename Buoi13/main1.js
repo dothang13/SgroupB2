e5c0b94478fda6ae3e490ebe182b6f730aa0c5ef
dothang13 <endiezzhang13@gmail.com>
2024-03-20 23:16:34 +0700

    hi

--- Buoi13/main1.js
+++ Buoi13/main1.js
@@ -1,27 +1,18 @@
-function register() {
-    const username = document.getElementById("Dang_ky-username").value;
-    const password = document.getElementById("Dang_ky-password").value;
+async function login(){
+    const username = document.getElementById('Dangnhap-username').value;
+    const password = document.getElementById('Dangnhap-password').value;
 
     let loginArray = JSON.parse(localStorage.getItem('loginArray')) || [];
-    const loginInfo = {username, password };
-
+    
     var count = 0;
     for (var i = 0; i < loginArray.length; i++){
         if(username == loginArray[i].username && password == loginArray[i].password) count++;
     }
-
-    if(count == 0) {
-        loginArray.push(loginInfo);
-
-        localStorage.setItem('lloginArray', JSON.stringify(loginArray));
-        window.location.href = "index2.html";
+    if (count) {
+        window.location.href = "https://youtu.be/x25387E3caw?si=JrVGjU3OsJ4k7CAl";
     }
-    else {
-        alert('Tài khoản đã tồn tại');
-        window.location.href = "index1.html";
+    else{
+        alert("Sai roi, nhap lai coi. Sai 3 lan khoa acc");
     }
 }
 
-document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("button").addEventListener("click", register)
-});
